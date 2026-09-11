0121dae1378935c01e6f3cf0730cb20b8476c7a6
Abderrazak Naceur <therzeos@gmail.com>
2026-09-11 16:36:25 +0200

    feat(ai): register replaceable production providers

--- backend/src/YoutubeStudio.Api/Program.cs
+++ backend/src/YoutubeStudio.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using YoutubeStudio.Api.Data;
 using YoutubeStudio.Api.Services.Production;
+using YoutubeStudio.Api.Services.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,15 @@ builder.Services.AddDbContext<YoutubeStudioDbContext>(options =>
 
 builder.Services.AddScoped<IProductionJobService, ProductionJobService>();
 builder.Services.AddHostedService<VideoProductionWorker>();
+
+builder.Services.AddScoped<IResearchProvider, PlaceholderResearchProvider>();
+builder.Services.AddScoped<IScriptProvider, PlaceholderScriptProvider>();
+builder.Services.AddScoped<IScenePlanProvider, PlaceholderScenePlanProvider>();
+builder.Services.AddScoped<IVoiceProvider, PlaceholderVoiceProvider>();
+builder.Services.AddScoped<IVisualProvider, PlaceholderVisualProvider>();
+builder.Services.AddScoped<ICaptionProvider, PlaceholderCaptionProvider>();
+builder.Services.AddScoped<IRenderProvider, PlaceholderRenderProvider>();
+builder.Services.AddScoped<IQaProvider, PlaceholderQaProvider>();
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection")!);
