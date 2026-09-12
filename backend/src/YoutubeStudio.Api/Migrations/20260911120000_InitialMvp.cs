2fa015c2883acb3b546edb467eaa23fa3b96fcbc
Abderrazak Naceur <therzeos@gmail.com>
2026-09-12 07:08:44 +0200

    fix(ci): make EF migrations discoverable by DbContext

--- backend/src/YoutubeStudio.Api/Migrations/20260911120000_InitialMvp.cs
+++ backend/src/YoutubeStudio.Api/Migrations/20260911120000_InitialMvp.cs
@@ -1,10 +1,13 @@
 using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
+using YoutubeStudio.Api.Data;
 
 #nullable disable
 
 namespace YoutubeStudio.Api.Migrations;
 
+[DbContext(typeof(YoutubeStudioDbContext))]
 [Migration("20260911120000_InitialMvp")]
 public partial class InitialMvp : Migration
 {
