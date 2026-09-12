8a7cefe0c603c45f415a7bd855efd2cf34ccecbc
Abderrazak Naceur <therzeos@gmail.com>
2026-09-13 01:53:55 +0200

    fix(ef): associate research migrations with DbContext

--- backend/src/YoutubeStudio.Api/Migrations/20260912145000_AddResearchEvidence.cs
+++ backend/src/YoutubeStudio.Api/Migrations/20260912145000_AddResearchEvidence.cs
@@ -1,10 +1,14 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+using YoutubeStudio.Api.Data;
 
 #nullable disable
 
 namespace YoutubeStudio.Api.Migrations;
 
+[DbContext(typeof(YoutubeStudioDbContext))]
 [Migration("20260912145000_AddResearchEvidence")]
 public partial class AddResearchEvidence : Migration
 {
