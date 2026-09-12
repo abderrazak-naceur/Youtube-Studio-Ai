a459f3381fbfad0632460460b6b4f9c61ce44d2b
Abderrazak Naceur <therzeos@gmail.com>
2026-09-12 13:07:46 +0200

    feat(v0.3): add persisted research project model

---
+++ backend/src/YoutubeStudio.Api/Models/ResearchProject.cs
@@ -0,0 +1,9 @@
+namespace YoutubeStudio.Api.Models;
+
+public sealed class ResearchProject : Entity
+{
+    public Guid WorkspaceId { get; set; }
+    public Guid OpportunityId { get; set; }
+    public string Status { get; set; } = "draft";
+    public Opportunity Opportunity { get; set; } = null!;
+}
