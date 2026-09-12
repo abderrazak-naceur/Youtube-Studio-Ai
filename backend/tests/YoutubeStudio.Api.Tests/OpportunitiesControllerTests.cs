7ec52494566b15d93379c727a1e3a832065d362c
Abderrazak Naceur <therzeos@gmail.com>
2026-09-12 12:51:00 +0200

    fix(v0.2): add missing framework imports to opportunity tests

--- backend/tests/YoutubeStudio.Api.Tests/OpportunitiesControllerTests.cs
+++ backend/tests/YoutubeStudio.Api.Tests/OpportunitiesControllerTests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using YoutubeStudio.Api.Controllers;
