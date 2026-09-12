e716605f495987cbe14fc653912b7d54ed0f5b95
Abderrazak Naceur <therzeos@gmail.com>
2026-09-12 07:39:29 +0200

    fix(db): import EF DbContext migration attribute

--- backend/src/YoutubeStudio.Api/Migrations/20260911143000_AddVideoProjects.cs
+++ backend/src/YoutubeStudio.Api/Migrations/20260911143000_AddVideoProjects.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using YoutubeStudio.Api.Data;
 
