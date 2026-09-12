d0edbba84342d4fae565cc9cd26bf991dd56cd96
Abderrazak Naceur <therzeos@gmail.com>
2026-09-12 09:52:30 +0200

    fix(v0.1): align EF snapshot identifier metadata

--- backend/src/YoutubeStudio.Api/Migrations/YoutubeStudioDbContextModelSnapshot.cs
+++ backend/src/YoutubeStudio.Api/Migrations/YoutubeStudioDbContextModelSnapshot.cs
@@ -14,6 +14,7 @@ partial class YoutubeStudioDbContextModelSnapshot : ModelSnapshot
     {
 #pragma warning disable 612, 618
         modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
+        modelBuilder.HasAnnotation("Relational:MaxIdentifierLength", 63);
         modelBuilder.HasAnnotation("Npgsql:PostgresModelCustomizer:PostgresVersion", new Version(17, 0));
         modelBuilder.HasPostgresExtension("vector");
 
